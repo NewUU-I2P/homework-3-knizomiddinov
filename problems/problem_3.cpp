96bf56cf82ab014ce37fc74d164920eb45187349
Kamoliddin <k.nizomiddinov@newuu.uz>
2023-11-13 10:33:12 +0500

    homework

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -3,6 +3,25 @@
 std::string problemSolution3(float height, char S) {
     // write your code here
 
+    if (S == 'M') {
+        if (height <= 1.70) {
+            std::cout << "Short";
+        } else if (height >= 1.70 && height < 1.85) {
+            std::cout << "Normal";
+        } else if (height >= 1.85) {
+            std::cout << "Tall";
+        }
+    } else if (S == 'F') {
+        if (height <= 1.60) {
+            std::cout << "Short";
+        } else if (height >= 1.60 && height < 1.75) {
+            std::cout << "Normal";
+        } else if (height >= 1.75) {
+            std::cout << "Tall";
+        }
+    }
+
+
     // use return to return your result
     // make use of control flow statements
 }
