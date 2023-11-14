37b0ccc622814e4f863cdea3b57f1daa0d69020d
Kamoliddin <k.nizomiddinov@newuu.uz>
2023-11-14 11:18:00 +0500

    homework

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -3,6 +3,18 @@
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
+    std::istringstream iss(macAddress);
+    int octet;
+
+    // Read the first octet to determine the type
+    iss >> std::hex >> octet;
+
+    if (iss.fail()) {
+        // Handle invalid input
+        return "Invalid MAC address";
+    }
+
+    return (octet % 2 == 0) ? "Unicast" : (octet == 0xFF) ? "Broadcast" : "Multicast";
 
     // make use of control flow statements
     // return result;
